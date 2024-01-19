8c1be13aae0dd28750a201c45783b955e2608db0
hasibul islam <sifathasib@gmail.com>
2024-01-20 01:46:02 +0600

    chapter 2 soln

---
+++ Chapter2/ce_loss.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+def categorical_ce(y,y_hat):
+    y , y_hat = np.array(y), np.array(y_hat)
+    ce = -np.sum(np.multiply(y,np.log10(y_hat)));
+    return ce 
+
+
+y = [0, 1, 0]
+y_hat = [0.02,0.97835,0.00165]
+ce_loss = categorical_ce(y=y, y_hat=y_hat)
+print("Cross-Entropy Loss = {}".format(ce_loss))
